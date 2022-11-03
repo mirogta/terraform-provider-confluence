5dfb7f1aa74b628f93563c409083024a2704743b
Miro Sommer <miro.sommer@samsara.com>
2022-11-03 13:30:34 +0000

    enable debugging

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/mirogta/terraform-provider-confluence/confluence"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: confluence.Provider})
+	var debugMode bool
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
+		ProviderFunc: confluence.Provider,
+	}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "mirogta/terraform-provider-confluence", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
